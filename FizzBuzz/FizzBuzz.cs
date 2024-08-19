78d5e24e93af3e143d884664407199a5f874ffac
Inge Amdal Halvorsen <inge.halvorsen@bouvet.no>
2024-08-19 15:19:27 +0200

    adds test for 5

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -9,7 +9,35 @@ public class FizzBuzz
 
     public string TransformNumber(int number)
     {
-        // this first run returns the simplest of answers
-        return "Fizz";
+        // We have two numbers, 3 and 5
+        // a simple switch case should do the trick, or an If loop
+        /**
+        Alternative 1
+        
+        switch (number)
+        {
+            case 3:
+            {
+                return "Fizz";
+            }
+            case 5:
+            {
+                return "Buzz";
+            }
+            // also needs a default, we'll leave this empty for now
+            default:
+            {
+                return "";
+            }
+        }
+        */
+        /**
+        Alternative 2
+        */
+        if (number == 3)
+        {
+            return "Fizz";
+        }
+        else return "Buzz";
     }
 }
