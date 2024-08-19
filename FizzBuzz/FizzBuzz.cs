ce0c3743ac06af32171b3e49e1adec5d7de1c7d4
Inge Amdal Halvorsen <inge.halvorsen@bouvet.no>
2024-08-19 14:58:54 +0200

    adds a comment to the code for easier reasding

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+namespace FizzBuzz;
+
+public class FizzBuzz
+{
+    public FizzBuzz()
+    {
+
+    }
+
+    public string TransformNumber(int number)
+    {
+        // this first run returns the simplest of answers
+        return "Fizz";
+    }
+}
